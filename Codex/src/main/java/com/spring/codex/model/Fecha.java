ad78995a9108bf2c841530a2832152b2313a1363
Black-MOAC <37340118+Black-MOAC@users.noreply.github.com>
2018-04-27 19:00:21 +0200

    Prestamo

---
+++ Codex/src/main/java/com/spring/codex/model/Fecha.java
@@ -0,0 +1,67 @@
+import java.util.Calendar;
+
+public class Fecha {
+	private int dia;
+	private int mes;
+	private int año;
+	private Calendar date = Calendar.getInstance();
+
+	// La fecha se inicia a la de hoy
+	public Fecha() {
+		dia = date.get(Calendar.DAY_OF_MONTH);
+		mes = date.get(Calendar.MONTH) + 1;
+		año = date.get(Calendar.YEAR);
+	}
+
+	// La fecha es la que es pasada por parametros
+	public Fecha(int d, int m, int a) {
+		establecerFecha(d, m, a);
+	}
+
+	public Fecha(Fecha f) {
+		dia = f.obtenerDia();
+		mes = f.obtenerMes();
+		año = f.obtenerAño();
+	}
+
+	public void establecerFecha(int d, int m, int a) {
+		if ((m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) && (d < 1 || d > 31)) {
+			throw new RuntimeException("Día erroneo.");
+		} else if ((m == 4 || m == 6 || m == 9 || m == 11) && (d < 1 || d > 30)) {
+			throw new RuntimeException("Día erroneo.");
+		} else if (m == 2) {
+			if (!bisiesto(a) && (d < 1 || d > 28)) {
+				throw new RuntimeException("Día erroneo.");
+			} else if (bisiesto(a) && (d < 1 || d > 29)) {
+				throw new RuntimeException("Día erroneo.");
+			}
+		} else if (m < 1 && m > 12) {
+			throw new RuntimeException("Mes erroneo");
+		}
+
+		dia = d;
+		mes = m;
+		año = a;
+	}
+
+	public int obtenerDia() {
+		return dia;
+	}
+
+	public int obtenerMes() {
+		return mes;
+	}
+
+	public int obtenerAño() {
+		return año;
+	}
+
+	private boolean bisiesto(int a) {
+		return a % 4 == 0 && (a % 100 != 0 || a % 400 == 0);
+	}
+
+	@Override
+	public String toString() {
+		return dia + "/" + mes + "/" + año;
+	}
+}
---
+++ Codex/src/main/java/com/spring/codex/model/Prestamo.java
@@ -0,0 +1,49 @@
+
+public class Prestamo {
+	private Fecha fecha_prestamo;
+	private Fecha fecha_devolucion;
+	
+	public Prestamo() {
+		fecha_prestamo = new Fecha();
+		fecha_devolucion = new Fecha();
+	}
+	
+	public Prestamo(Fecha f_prestamo, Fecha f_devolucion) {
+		establecerPrestamo(f_prestamo);
+		establecerDevolucion(f_devolucion);
+	}
+	
+	public Prestamo(int d_prest, int m_prest, int a_prest, int d_devol, int m_devol, int a_devol) {
+		establecerPrestamo(d_prest, m_prest, a_prest);
+		establecerDevolucion(d_devol, m_devol, a_devol);
+	}
+	
+	public void establecerPrestamo(Fecha f_prestamo) {
+		fecha_prestamo = new Fecha(f_prestamo);
+	}
+	
+	public void establecerPrestamo(int d, int m, int a) {
+		fecha_prestamo = new Fecha(d, m, a);
+	}
+	
+	public void establecerDevolucion(Fecha f_devolucion) {
+		fecha_devolucion = new Fecha(f_devolucion);
+	}
+	
+	public void establecerDevolucion(int d, int m, int a) {
+		fecha_devolucion = new Fecha(d, m, a);
+	}
+	
+	public String obtenerPrestamo() {
+		return fecha_prestamo.toString();
+	}
+	
+	public String obtenerDevolucion() {
+		return fecha_devolucion.toString();
+	}
+	
+	@Override
+	public String toString() {
+		return "Fecha del prestamo: " + fecha_prestamo.toString() + "\n" + "Fecha de la devolucion: " + fecha_devolucion.toString();
+	}
+}
