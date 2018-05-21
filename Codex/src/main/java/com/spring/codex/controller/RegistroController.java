a8a889d4e201e1cc8cfc0d2246ff7918110d0e94
AlejandroFuster <AlejandroFuster@users.noreply.github.com>
2018-05-21 16:17:22 +0200

    Mejorada la implementación del Controlador del registro

--- Codex/src/main/java/com/spring/codex/controller/RegistroController.java
+++ Codex/src/main/java/com/spring/codex/controller/RegistroController.java
@@ -1,43 +1,50 @@
 package com.spring.codex.controller;
 
+import com.spring.codex.model.Usuario;
+import com.spring.codex.model.UsuarioService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.spring.codex.model.Usuario;
-import com.spring.codex.model.UsuarioService;
+import javax.validation.Valid;
 
 @Controller
+@RequestMapping("/registro")
 public class RegistroController {
-	
-	@Autowired
-	private UsuarioService usuarioService;
-	
-	
-    @RequestMapping(value = "/registro", method = RequestMethod.GET)
-    public String registro(Model model) {
-        model.addAttribute("registro", new Usuario());
 
+    @Autowired
+    private UsuarioService userService;
+
+    @ModelAttribute("user")
+    public Usuario Usuario() {
+        return new Usuario();
+    }
+
+    @GetMapping
+    public String muestrRegistro(Model model) {
         return "registro";
     }
-    
-    @RequestMapping(value = "/registro", method = RequestMethod.POST)
-    public String registration(@ModelAttribute("registro") Usuario userForm, BindingResult bindingResult, Model model) {
 
-        if (bindingResult.hasErrors()) {
+    @PostMapping
+    public String registerUserAccount(@ModelAttribute("user") @Valid Usuario user, 
+                                      BindingResult result){
+
+        Usuario existe = userService.findByEmail(user.getEmail());
+        if (existe != null){
+            result.rejectValue("email", null, "¡Oops! Ya hay un usuario con ese correo. ");
+        }
+
+        if (result.hasErrors()){
             return "registro";
         }
 
-        usuarioService.saveUsuario(userForm);
-
+        userService.saveUsuario(user);
         return "redirect:/";
     }
-
-
-
 
 }
