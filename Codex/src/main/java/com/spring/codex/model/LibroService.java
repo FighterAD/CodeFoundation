6201ab92dac5dafdd7e8da9a87a748c1f05713d3
AlejandroFuster <AlejandroFuster@users.noreply.github.com>
2018-05-25 16:51:09 +0200

    Resuelto problema editando libros y mejorado diseño

--- Codex/src/main/java/com/spring/codex/model/LibroService.java
+++ Codex/src/main/java/com/spring/codex/model/LibroService.java
@@ -18,10 +18,10 @@ public class LibroService {
 
 	/*---Actualiza un libro---*/
 	public void update(Libro libro) {
-		Libro l = repository.getOne(libro.getIsbn13());
-		l.setEditor(libro.getEditor());
+		Libro l = repository.findByIsbn13(libro.getIsbn13());
 		l.setAutor(libro.getAutor());
 		l.setNombre(libro.getNombre());
+		System.out.println(libro.getAutor());
 		repository.saveAndFlush(l);
 	}
 
@@ -47,7 +47,6 @@ public class LibroService {
 	public void delete(long isbn13) {
 		repository.deleteById(isbn13);
 	}
-	
 
 
 }
