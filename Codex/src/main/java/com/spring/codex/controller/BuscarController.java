66f206221c8b51bb3bc614de18df26272c521d82
Black-MOAC <37340118+Black-MOAC@users.noreply.github.com>
2018-05-30 19:36:12 +0200

    Add files via upload

--- Codex/src/main/java/com/spring/codex/controller/BuscarController.java
+++ Codex/src/main/java/com/spring/codex/controller/BuscarController.java
@@ -1,14 +1,46 @@
 package com.spring.codex.controller;
 
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.spring.codex.model.Libro;
+import com.spring.codex.model.LibroService;
 
 @Controller
 public class BuscarController {
+	@Autowired
+	LibroService libroService;
 	
+	Libro aux;
+
 	@GetMapping("/buscar")
-	public String buscarLibro() {
+	public String buscar(Libro l, Model model) {
+		aux = libroService.getByName("Yerma");
+		model.addAttribute("libros", libroService.getAll());
+		model.addAttribute("libro", libroService.getByName(aux.getNombre()));
 		return "buscar";
 	}
-	
+
+	@PostMapping("/buscar")
+	public String save(@Valid Libro l, BindingResult result, Model model) {
+		if(libroService.getByName(l.getNombre()) != null) {
+			aux.setAnyo(l.getAnyo());
+			aux.setAutor(l.getAutor());
+			aux.setEditor(l.getEditor());
+			aux.setIsbn13(l.getIsbn13());
+			aux.setNombre(l.getNombre());
+			
+			model.addAttribute("libros", libroService.getAll());
+			model.addAttribute("libro", libroService.getByName(l.getNombre()));
+		}else {
+			model.addAttribute("libros", libroService.getAll());
+			model.addAttribute("libro", libroService.getByName(aux.getNombre()));
+		}
+		
+		return "buscar";
+	}
 }
--- Codex/src/main/java/com/spring/codex/model/Prestamo.java
+++ Codex/src/main/java/com/spring/codex/model/Prestamo.java
@@ -1,57 +1,55 @@
 
 package com.spring.codex.model;
 
-import javax.persistence.*;
+import java.util.Date;
 
 //@Entity
 public class Prestamo {
 	/*
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)*/
+	 * @Id
+	 * 
+	 * @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 */
 	private int idPrestamo;
-	private Fecha fecha_prestamo;
-	private Fecha fecha_devolucion;
-	
-	public Prestamo() {
-		this(new Fecha(), new Fecha());
+	private String prestatario;
+	private Date fecha_prestamo;
+	private Date fecha_devolucion;
+
+	public Prestamo(String n, Date fp, Date fd) {
+		prestatario = n;
+		fecha_prestamo = fp;
+		fecha_devolucion = fd;
 	}
-	
-	public Prestamo(Fecha f_prestamo, Fecha f_devolucion) {
-		establecerPrestamo(f_prestamo);
-		establecerDevolucion(f_devolucion);
+
+	public int getIdPrestamo() {
+		return idPrestamo;
 	}
-	
-	public Prestamo(int d_prest, int m_prest, int a_prest, int d_devol, int m_devol, int a_devol) {
-		establecerPrestamo(d_prest, m_prest, a_prest);
-		establecerDevolucion(d_devol, m_devol, a_devol);
+
+	public void setIdPrestamo(int idPrestamo) {
+		this.idPrestamo = idPrestamo;
 	}
-	
-	public void establecerPrestamo(Fecha f_prestamo) {
-		fecha_prestamo = new Fecha(f_prestamo);
+
+	public String getPrestatario() {
+		return prestatario;
 	}
-	
-	public void establecerPrestamo(int d, int m, int a) {
-		fecha_prestamo = new Fecha(d, m, a);
+
+	public void setPrestatario(String prestatario) {
+		this.prestatario = prestatario;
 	}
-	
-	public void establecerDevolucion(Fecha f_devolucion) {
-		fecha_devolucion = new Fecha(f_devolucion);
+
+	public Date getFecha_prestamo() {
+		return fecha_prestamo;
 	}
-	
-	public void establecerDevolucion(int d, int m, int a) {
-		fecha_devolucion = new Fecha(d, m, a);
+
+	public void setFecha_prestamo(Date fecha_prestamo) {
+		this.fecha_prestamo = fecha_prestamo;
 	}
-	
-	public String obtenerPrestamo() {
-		return fecha_prestamo.toString();
+
+	public Date getFecha_devolucion() {
+		return fecha_devolucion;
 	}
-	
-	public String obtenerDevolucion() {
-		return fecha_devolucion.toString();
-	}
-	
-	@Override
-	public String toString() {
-		return "Fecha del prestamo: " + fecha_prestamo.toString() + "\n" + "Fecha de la devolucion: " + fecha_devolucion.toString();
+
+	public void setFecha_devolucion(Date fecha_devolucion) {
+		this.fecha_devolucion = fecha_devolucion;
 	}
 }
